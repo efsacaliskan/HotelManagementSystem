7f7f5fd7d76eeaf82930ee304667963a208ba455
efsacaliskan <100141705+efsacaliskan@users.noreply.github.com>
2024-01-11 21:45:01 +0300

    Update RoomController.java

--- hotel-management/src/main/java/com/ozyegin/hotelmanagement/controller/RoomController.java
+++ hotel-management/src/main/java/com/ozyegin/hotelmanagement/controller/RoomController.java
@@ -1,11 +1,11 @@
 package com.ozyegin.hotelmanagement.controller;
 
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +22,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/rooms")
 public class RoomController {
